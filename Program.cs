78537ef694111fad584fd4678b4f1d836142f26a
nandun.withanachchi <nandun.withanachchi@gmail.com>
2021-11-15 06:14:40 +0530

    Further modifications

--- Program.cs
+++ Program.cs
@@ -1,16 +1,51 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Text.Json;
 
 namespace RateLimitApp
 {
     class Program
     {
-        static void Main(string[] args)
+        private static readonly HttpClient client = new HttpClient();
+        private static async Task GetRateLimit()
         {
-            //string github_pat;
-            Console.WriteLine("Hello World!");
-            //Console.WriteLine("Enter Github PAT");
-            //github_pat = Console.ReadLine();
-            //Console.WriteLine($"Your result: {github_pat} = " + github_pat);
+            string github_pat;
+            Console.WriteLine("Enter Github PAT");
+            github_pat = Console.ReadLine();
+            //github_pat = "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";
+
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            client.DefaultRequestHeaders.Add("Authorization", "Bearer " + github_pat);
+
+            var streamTask = client.GetStreamAsync("https://api.github.com/rate_limit");
+            var rate = await JsonSerializer.DeserializeAsync<Rate>(await streamTask);
+            Console.WriteLine(rate.resources.core.limit);
+            Console.WriteLine(rate.resources.core.remaining);
+            if (rate.resources.core.remaining/rate.resources.core.limit < .1)
+            {
+                Environment.Exit(1);
+            }
+            else
+            {
+                Environment.Exit(0);
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                await GetRateLimit();
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("ERROR: " + err.Message);
+            }
         }
     }
 }
---
+++ Rate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+namespace RateLimitApp
+{
+    class Rate
+    {
+        public res resources  { get; set; }
+        public class res
+        {
+            public cores core { get; set; }
+
+            public class cores 
+            {
+                public double limit { get; set; }
+                public double remaining { get; set; }
+            }
+        }
+    }
+}
